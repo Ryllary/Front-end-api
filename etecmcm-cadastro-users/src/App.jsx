53d24a8fdd49d25afe6a75f0373352dec96c03e7
ryllary <victoriaryllary103@gmail.com>
2025-09-16 11:11:21 -0300

    save

---
+++ etecmcm-cadastro-users/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      BOLSONAROOOOOOOOOOOOOOOOOOOOOOOOOOOOO!
+    </div>
+  );
+}
+
+export default App;
+
+//.app {
+
+  //background-size: cover;
+  //background-position: center;
+  //height: 100vh;
+  //margin: 0;
+  //padding: 0;
+
+//}
